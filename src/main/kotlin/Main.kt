d4b48ce84bfb57755445f72720f23c5b9312ab04
Denis Vetokhin <Tornado-000@rambler.ru>
2022-04-23 20:22:03 +0300

    old vertion

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,120 @@
+fun main() {
+
+    val postOne = Post(
+        id = 5,
+        ownerId = 666,
+        fromId = 111,
+        createdBy = 5656,
+        date = 12042022,
+        text = "Sun shine and it is okey",
+        replyOwnerId = 7890,
+        replyPostId = 2346,
+        friendsOnly = false,
+        comments = Post.Comments(count = 18, canPost = true, groupsCanPost = false, canClose = true, canOpen = true),
+        copyright = "newspaper Pravda",
+        likes = Post.Likes(count = 25, userLikes = true, canLike = false, canPublish = true),
+        reposts = Post.Reposts(count = 3, userReposted = true),
+        viewsCount = 15,
+        postType = "post",
+        signerId = 8965,
+        canPin = true,
+        canDelete = true,
+        canEdit = true,
+        isPinned = true,
+        markedAsAds = false,
+        isFavorite = true,
+        postponedId = 0
+    )
+
+    val postTwo = Post(
+        id = 6,
+        ownerId = 666,
+        fromId = 111,
+        createdBy = 5656,
+        date = 12042022,
+        text = "Sun shine and it is okey",
+        replyOwnerId = 7890,
+        replyPostId = 2346,
+        friendsOnly = false,
+        comments = Post.Comments(count = 18, canPost = true, groupsCanPost = false, canClose = true, canOpen = true),
+        copyright = "newspaper Pravda",
+        likes = Post.Likes(count = 25, userLikes = true, canLike = false, canPublish = true),
+        reposts = Post.Reposts(count = 3, userReposted = true),
+        viewsCount = 15,
+        postType = "post",
+        signerId = 8965,
+        canPin = true,
+        canDelete = true,
+        canEdit = true,
+        isPinned = true,
+        markedAsAds = false,
+        isFavorite = true,
+        postponedId = 0
+    )
+    val postThree = Post(
+        id = 8,
+        ownerId = 666,
+        fromId = 111,
+        createdBy = 5656,
+        date = 12042022,
+        text = "Sun shine and it is okey",
+        replyOwnerId = 7890,
+        replyPostId = 2346,
+        friendsOnly = false,
+        comments = Post.Comments(count = 18, canPost = true, groupsCanPost = false, canClose = true, canOpen = true),
+        copyright = "newspaper Pravda",
+        likes = Post.Likes(count = 25, userLikes = true, canLike = false, canPublish = true),
+        reposts = Post.Reposts(count = 3, userReposted = true),
+        viewsCount = 15,
+        postType = "post",
+        signerId = 8965,
+        canPin = true,
+        canDelete = true,
+        canEdit = true,
+        isPinned = true,
+        markedAsAds = false,
+        isFavorite = true,
+        postponedId = 0
+    )
+
+    var postFour = Post(
+        id = 2,
+        ownerId = 999,
+        fromId = 999,
+        createdBy = 999,
+        date = 12042022,
+        text = "Sun shine and it is okey",
+        replyOwnerId = 7890,
+        replyPostId = 2346,
+        friendsOnly = false,
+        comments = Post.Comments(count = 18, canPost = true, groupsCanPost = false, canClose = true, canOpen = true),
+        copyright = "newspaper Pravda",
+        likes = Post.Likes(count = 25, userLikes = true, canLike = false, canPublish = true),
+        reposts = Post.Reposts(count = 3, userReposted = true),
+        viewsCount = 15,
+        postType = "post",
+        signerId = 8965,
+        canPin = true,
+        canDelete = true,
+        canEdit = true,
+        isPinned = true,
+        markedAsAds = false,
+        isFavorite = true,
+        postponedId = 0
+    )
+
+    val wall = WallService()
+
+    val controlOne = wall.add(postOne)
+    val controlTwo = wall.add(postTwo)
+    val controlThree = wall.add(postThree)
+
+    wall.update(postFour)
+
+    println(controlOne.id)
+    println(controlTwo.id)
+    println(controlThree.id)
+    println(postFour.id)
+    println(wall.update(postFour))
+
+}
---
+++ src/main/kotlin/Post.kt
@@ -0,0 +1,44 @@
+data class Post(
+    val id: Int,
+    val ownerId: Int,
+    val fromId: Int,
+    val createdBy: Int,
+    val date: Int,
+    val text: String,
+    val replyOwnerId: Int,
+    val replyPostId: Int,
+    val friendsOnly: Boolean,
+    val comments: Comments,
+    val copyright: String,
+    val likes: Likes,
+    val reposts: Reposts,
+    val viewsCount: Int,
+    val postType: String,
+    val signerId: Int,
+    val canPin: Boolean,
+    val canDelete: Boolean,
+    val canEdit: Boolean,
+    val isPinned: Boolean,
+    val markedAsAds: Boolean,
+    val isFavorite: Boolean,
+    val postponedId: Int
+) {
+    data class Comments(
+        val count: Int,
+        val canPost: Boolean,
+        val groupsCanPost: Boolean,
+        val canClose: Boolean,
+        val canOpen: Boolean
+    )
+    data class Reposts(
+        val count: Int,
+        val userReposted: Boolean
+    )
+    data class Likes(
+        val count: Int,
+        val userLikes: Boolean,
+        val canLike: Boolean,
+        val canPublish: Boolean
+    )
+}
+
